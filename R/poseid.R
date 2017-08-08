40c7942d90a61a4d50132a76f262c6fa8209d3cc
Lucie Contamin <contamin.lucie@gmail.com>
2017-08-08 14:54:37 +0700

    Add poseid documentation

---
+++ R/poseid.R
@@ -0,0 +1,16 @@
+#' poseid: Population-based Observational Studies of the Epidemiology of Infectious Diseases
+#'
+#' The marc package provide functions of visualizing and analysing
+#' population-based observational data in epidemiology.
+#'
+#' @importFrom dplyr select_if
+#' @importFrom dplyr filter
+#' @importFrom sp merge
+#' @importFrom magrittr %>%
+#' @importFrom magrittr %<>%
+#' @importFrom classInt classIntervals
+#' @importFrom classInt findColours
+#'
+#' @docType package
+#' @name poseid
+NULL
