65fd0a774a2d1d4499d684c8838cbbafdb844ef4
Lucie Contamin <contamin.lucie@gmail.com>
2017-08-08 16:05:48 +0700

    update dependencies

--- R/poseid.R
+++ R/poseid.R
@@ -3,13 +3,14 @@
 #' The marc package provide functions of visualizing and analysing
 #' population-based observational data in epidemiology.
 #'
-#' @importFrom dplyr select_if
-#' @importFrom dplyr filter
+#' @importFrom dplyr select_if filter
 #' @importFrom sp merge
-#' @importFrom magrittr %>%
-#' @importFrom magrittr %<>%
-#' @importFrom classInt classIntervals
-#' @importFrom classInt findColours
+#' @importFrom magrittr %>% %<>%
+#' @importFrom classInt classIntervals findColours
+#' @importFrom grDevices colorRampPalette heat.colors
+#' @importFrom graphics box image layout locator par rect segments strwidth text
+#' @importFrom stats na.omit
+#' @importFrom utils tail
 #'
 #' @docType package
 #' @name poseid
