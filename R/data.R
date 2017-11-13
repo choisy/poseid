4f01fcba480a49ae0d2134a7e38449d8d59903b7
Lucie Contamin <contamin.lucie@gmail.com>
2017-11-13 17:52:12 +0700

    move infections_dates to data

---
+++ R/data.R
@@ -0,0 +1,8 @@
+#' Vector of dates
+#'
+#' A vector containing multiple dates in the format "%Y-%m-%d", used as example
+#' for the \code{ll2incidence} function
+#'
+#' @format A vector of 7018 dates.
+#'
+"infections_dates"
